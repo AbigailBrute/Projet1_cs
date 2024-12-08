eb79d8d89647559b439c17ee6e1f8be59295e447
Abigail Brute <bruteabigail@gmail.com>
2024-12-08 19:02:05 +0100

    code nettoyer

--- obj/Debug/net8.0/Projet1 C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projet1 C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109667bcc4024ddd13dcb4c1017af84bdf8bc6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198b05605e340ba54ad753faee48443ad892e271")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
