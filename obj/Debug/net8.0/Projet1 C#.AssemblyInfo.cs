b9e45d86b5f0787fbc31b518cfe6a4f089ac8205
Abigail Brute <bruteabigail@gmail.com>
2024-12-08 19:12:10 +0100

    nettoyage code

--- obj/Debug/net8.0/Projet1 C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projet1 C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198b05605e340ba54ad753faee48443ad892e271")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36aa3123eb43d4efa8e043f0424ac34bc8bda258")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
